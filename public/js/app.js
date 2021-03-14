61df2cc1d0d6da3c5498b3f234e5bdb2f82d4cc7
Manuel <carlosd99m@gmail.com>
2021-03-14 02:11:13 -0600

    :fire:

--- public/js/app.js
+++ public/js/app.js
@@ -49886,8 +49886,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/manuel/Documents/Laravel/recetaslaravel/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/manuel/Documents/Laravel/recetaslaravel/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! F:\Laravel\recetaslaravel\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! F:\Laravel\recetaslaravel\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
